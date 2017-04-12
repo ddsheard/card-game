1f6792eb6a4afb44c17a411294a5a4edf2c4a305
ddsheard <ddsheard@gmail.com>
2017-04-12 18:38:25 -0400

    still working

--- test/blackjackTest.js
+++ test/blackjackTest.js
@@ -1,11 +1,13 @@
 // Aspects of Blackjack to Model
-//
-//   - A game (for our purposes) has one player and a dealer
-//   - At the start of the game, the player gets two cards and so does the dealer
+
 //   - The player can ask for another card
 //   - The player can repeat that request as many times as desired
 //   - Once the player doesn't want any more cards, the dealer will ask for more cards until the dealer's hand is worth at least 17
 //   - For today, assume that an Ace is worth 1, a face card (Jack, Queen, or King) is worth 10, and any other card is worth its number
+
+// COMPLETED
+//1. A game (for our purposes) has one player and a dealer
+//2. At the start of the game, the player gets two cards and so does the dealer
 
 
 const assert = require('assert');
@@ -13,34 +15,52 @@ const Deck = require('../lib/Deck');
 const Card = require('../lib/Card');
 const Blackjack = require('../lib/Blackjack');
 
+//Assigning a player and a dealer
 describe('Blackjack', function (){
   it('has a player', function () {
-    let game = new Blackjack('Monkey');
-    assert.equal(game.player, 'Monkey');
+    let game = new Blackjack('Chaudia');
+    assert.equal(game.player, 'Chaudia');
   });
   it('has a dealer', function(){
-    let game = new Blackjack('Monkey', 'Giraffe');
-    assert.equal(game.dealer, 'Giraffe')
+    let game = new Blackjack('Chaudia', 'Jasmine');
+    assert.equal(game.dealer, 'Jasmine')
   });
   it('has a deck of cards', function(){
-    let game = new Blackjack('Monkey', 'Giraffe');
+    let game = new Blackjack('Chaudia', 'Jasmine');
     assert(52, game.deck.cards.length);
   })
 });
 
+//Each player gets dealt two cards
 describe('#gameStartsNow()', function() {
-  it('player gets two cards', function (){
-    let game = new Blackjack ('Monkey', 'Giraffe');
+  it('player gets two cards', function(){
+    let game = new Blackjack ('Chaudia', 'Jasmine');
     let gameStartsNow = game.gameStartsNow();//starts game
-    assert.equal(game.player, 'Monkey');
-    // assert.equal(50, game.cards.length);
+    assert.equal(game.playerGets.length, 2);//test player got two cards
   });
   it('dealer gets two cards', function(){
-    let game = new Blackjack ('Monkey', 'Giraffe');
+    let game = new Blackjack ('Chaudia', 'Jasmine');
     let gameStartsNow = game.gameStartsNow();
-    assert.equal(game.dealer, 'Giraffe');
+    assert.equal(game.dealerGets.length, 2);
   })
 });
+
+//   - The player can ask for another card
+describe('#additionalCards', function(){
+  it('player would like two additional cards', function(){
+    let game = new Blackjack ('Chaudia', 'Jasmine');
+    assert.equal(game.playerGets.length, 4)
+  })
+})
+
+
+//Give me another please
+// describe('#twoMorePlease'), funciton(){
+//   it('player needs two more cards' function(){
+//     let game = new Card
+//   })
+// }
+
 
 
 // describe(''), function() {
